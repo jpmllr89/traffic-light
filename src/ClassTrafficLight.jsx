ec4c51b2e0f4c2851677bf516fe544ac7afa7deb
Jesse Miller <jpmllr89@gmail.com>
2023-12-28 21:50:46 -0500

    first commit

--- src/ClassTrafficLight.jsx
+++ src/ClassTrafficLight.jsx
@@ -1,17 +1,37 @@
 import { Component } from "react";
 
 export class ClassTrafficLight extends Component {
+  state = {
+    color1: "red",
+    color2: "black",
+    color3: "black",
+  };
+
+  nextColor = () => {
+    const { color1, color2, color3 } = this.state;
+    if (color1 === "red") {
+      this.setState({ color1: "black", color2: "yellow" });
+    } else if (color2 === "yellow") {
+      this.setState({ color2: "black", color3: "green" });
+    } else if (color3 === "green") {
+      this.setState({ color3: "black", color1: "red" });
+    }
+  };
+
   render() {
+    const { color1, color2, color3 } = this.state;
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
           {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          <div className={`circle ${color1}`}></div>
+          <div className={`circle ${color2}`}></div>
+          <div className={`circle ${color3}`}></div>
         </div>
-        <button className="next-state-button">Next State</button>
+        <button className="next-state-button" onClick={this.nextColor}>
+          Next State
+        </button>
       </div>
     );
   }
--- src/FunctionalTrafficLight.jsx
+++ src/FunctionalTrafficLight.jsx
@@ -1,17 +1,34 @@
+import { useState } from "react";
+
 export const FunctionalTrafficLight = () => {
+  const [color1, setColor1] = useState("red");
+  const [color2, setColor2] = useState("black");
+  const [color3, setColor3] = useState("black");
+
+  const nextColor = () => {
+    if (color1 === "red") {
+      setColor1("black");
+      setColor2("yellow");
+    } else if (color2 === "yellow") {
+      setColor2("black");
+      setColor3("green");
+    } else if (color3 === "green") {
+      setColor3("black");
+      setColor1("red");
+    }
+  };
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
         {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <div className={`circle ${color1}`}></div>
+        <div className={`circle ${color2}`}></div>
+        <div className={`circle ${color3}`}></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button className="next-state-button" onClick={nextColor}>
+        Next State
+      </button>
     </div>
   );
 };
-
-
-
