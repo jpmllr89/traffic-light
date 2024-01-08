f169092abf4305c8431c1fcedce4b55ba777bf75
Jesse Miller <jpmllr89@gmail.com>
2024-01-08 18:14:51 -0500

    made changes according to loom vid

--- src/ClassTrafficLight.jsx
+++ src/ClassTrafficLight.jsx
@@ -2,32 +2,36 @@ import { Component } from "react";
 
 export class ClassTrafficLight extends Component {
   state = {
-    color1: "red",
+    color1: "green",
     color2: "black",
     color3: "black",
   };
 
   nextColor = () => {
     const { color1, color2, color3 } = this.state;
-    if (color1 === "red") {
+    if (color1 === "green") {
       this.setState({ color1: "black", color2: "yellow" });
     } else if (color2 === "yellow") {
-      this.setState({ color2: "black", color3: "green" });
-    } else if (color3 === "green") {
-      this.setState({ color3: "black", color1: "red" });
+      this.setState({ color2: "black", color3: "red" });
+    } else if (color3 === "red") {
+      this.setState({ color3: "black", color1: "green" });
     }
   };
 
   render() {
     const { color1, color2, color3 } = this.state;
+    const colors = [color3, color2, color1];
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
           {/* Background color can be black | yellow | red | green */}
-          <div className={`circle ${color1}`}></div>
+          {colors.map((color, index) => (
+            <div key={index} className={`circle ${color}`}></div>
+          ))}
+          {/* <div className={`circle ${color3}`}></div>
           <div className={`circle ${color2}`}></div>
-          <div className={`circle ${color3}`}></div>
+          <div className={`circle ${color1}`}></div> */}
         </div>
         <button className="next-state-button" onClick={this.nextColor}>
           Next State
--- src/FunctionalTrafficLight.jsx
+++ src/FunctionalTrafficLight.jsx
@@ -1,32 +1,47 @@
 import { useState } from "react";
 
-export const FunctionalTrafficLight = () => {
-  const [color1, setColor1] = useState("red");
-  const [color2, setColor2] = useState("black");
-  const [color3, setColor3] = useState("black");
+const lightSystem = [
+  ["black", "black", "green"],
+  ["black", "yellow", "black"],
+  ["red", "black", "black"],
+]
 
-  const nextColor = () => {
-    if (color1 === "red") {
-      setColor1("black");
-      setColor2("yellow");
-    } else if (color2 === "yellow") {
-      setColor2("black");
-      setColor3("green");
-    } else if (color3 === "green") {
-      setColor3("black");
-      setColor1("red");
+export const FunctionalTrafficLight = () => {
+  const [light, setLight] = useState(0);
+
+
+  
+  // const nextColor = () => {
+  //   if (color1 === "red") {
+  //     setColor1("black");
+  //     setColor2("yellow");
+  //   } else if (color2 === "yellow") {
+  //     setColor2("black");
+  //     setColor3("green");
+  //   } else if (color3 === "green") {
+  //     setColor3("black");
+  //     setColor1("red");
+  //   }
+  // };
+
+  const handleLight = () => {
+    if (light === lightSystem.length - 1){
+      setLight(0);
+    }else{
+      setLight(light + 1);
     }
-  };
+  }
+
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
         {/* Background color can be black | yellow | red | green */}
-        <div className={`circle ${color1}`}></div>
-        <div className={`circle ${color2}`}></div>
-        <div className={`circle ${color3}`}></div>
+        {lightSystem[light].map((color, index) => (
+          <div key={index} className={`circle ${color}`}></div>
+        ))}
       </div>
-      <button className="next-state-button" onClick={nextColor}>
+      <button className="next-state-button" onClick={handleLight}>
         Next State
       </button>
     </div>
